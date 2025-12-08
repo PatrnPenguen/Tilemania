29338ca6ab3fbc89b6e29a135a1d891cc93a2370
Furkan Karadas <42517177+PatrnPenguen@users.noreply.github.com>
2025-12-08 12:27:44 +0300

    lessons 70-72

--- Assets/2-Scripts/PlayerMovement.cs
+++ Assets/2-Scripts/PlayerMovement.cs
@@ -5,16 +5,21 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     Vector2 moveInput;
+    Animator animator;
     
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpForce = 15f;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value)
@@ -22,10 +27,26 @@ public class PlayerMovement : MonoBehaviour
         moveInput = value.Get<Vector2>(); 
     }
 
+    void OnJump(InputValue value)
+    {
+        rb.linearVelocity += new Vector2(0, jumpForce);
+    }
+
     void Run()
     {
         Vector2 playerVelocity = new Vector2(moveInput.x*runSpeed, rb.linearVelocity.y);
         rb.linearVelocity = playerVelocity;
-        
+        animator.SetBool("isRunning", Mathf.Abs(rb.linearVelocity.x) > Mathf.Epsilon);
     }
+
+    void FlipSprite()
+    {
+       bool hasHorizontalSpeed = Mathf.Abs(rb.linearVelocity.x) > Mathf.Epsilon;
+       if (hasHorizontalSpeed)
+       {
+           transform.localScale = new Vector2(Mathf.Sign(rb.linearVelocity.x), 1f);
+       }
+    }
+
+    
 }
