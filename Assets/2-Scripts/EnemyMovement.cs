43bc21964a1f511bc3fe8781be10e5059ec706c2
Buğra Furkan Karadaş <bfK.2002@hotmail.com>
2025-12-31 17:50:22 +0300

    enemy

---
+++ Assets/2-Scripts/EnemyMovement.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    [SerializeField] private float moveSpeed = 1f;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        _rb.linearVelocity = new Vector2 (moveSpeed, _rb.linearVelocity.y);
+        print(_rb.linearVelocity);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        moveSpeed = -moveSpeed;
+        EnemyFlip();
+    }
+
+    private void EnemyFlip()
+    {
+        transform.localScale = new Vector3(-Mathf.Sign(_rb.linearVelocity.x), 1f, 1f);
+    }
+}
